54d7a103f183548b5a6b7ec5d9bdf84e947213a1
jorses <15893473+jorses@users.noreply.github.com>
2025-05-14 18:53:09 +0200

    Update ProfileSection.tsx

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -9,7 +9,7 @@ const ProfileSection = () => {
       <Card>
         <CardContent className="pt-6">
           <p className="text-sm">
-I am a Data Scientist, Software Engineer, and PhD candidate in Computer Science. My research specializes in Tabular Reasoning with Large Language Models. In addition, I bring over five years of hands-on experience in designing, implementing, and delivering data-driven solutions to complex challenges across both software engineering and data science          </p>
+I am a Data Scientist, Software Engineer, and PhD candidate in Computer Science. My research specializes in Tabular Reasoning with Large Language Models. In addition, I bring over five years of hands-on experience in designing, implementing, and delivering data-driven solutions to complex challenges across both software engineering and data science.          </p>
         </CardContent>
       </Card>
     </div>
